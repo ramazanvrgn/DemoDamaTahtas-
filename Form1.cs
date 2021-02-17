17c285d5539e25bd4fed46f2b0db9d090dab9da8
DEVLET-İ OSMANİYYE <DEVLET- OSMANYYE@DESKTOP-SB0HQ78 MINGW64>
2021-02-17 23:45:13 +0300

    Proje dosyası ekle.

---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoDamaTahtası
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Button[,] buttons = new Button[8, 8];
+            int left = 0;
+            int top = 0;
+
+            for (int i = 0; i < buttons.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < buttons.GetUpperBound(1); j++)
+                {
+                    buttons[i, j]= new Button();
+                    buttons[i, j].Width = 100;
+                    buttons[i, j].Height = 100;
+                    buttons[i, j].Left = left;
+                    buttons[i, j].Top = top;
+                    left += 100;
+                    if ((i+j)%2==0)
+                    {
+                        buttons[i, j].BackColor = Color.DarkGray;
+                    }
+                    else
+                    {
+                        buttons[i, j].BackColor = Color.White;
+                    }
+                    this.Controls.Add(buttons[i, j]);
+                }
+                left = 0;
+                top += 100;
+
+            }
+        }
+    }
+}
